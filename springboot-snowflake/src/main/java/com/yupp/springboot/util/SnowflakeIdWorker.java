4395e2b2475fb50f4f67679e6d9d6fa9cf583e83
yupp <yupp@yiche.com>
2020-05-19 18:30:18 +0800

    解决找不到主加载类

---
+++ springboot-snowflake/src/main/java/com/yupp/springboot/util/SnowflakeIdWorker.java
@@ -0,0 +1,8 @@
+package com.yupp.springboot.util;
+
+/**
+ * @author yupp
+ * @create 2020/5/19 13:42
+ */
+public class SnowflakeIdWorker {
+}
---
+++ springboot-snowflake/src/main/java/com/yupp/springboot/util/T.java
@@ -0,0 +1,8 @@
+package com.yupp.springboot.util;
+
+/**
+ * @author yupp
+ * @create 2020/5/19 17:23
+ */
+public class T {
+}
